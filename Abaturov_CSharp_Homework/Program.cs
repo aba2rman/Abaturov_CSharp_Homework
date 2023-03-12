b6c578367ee87723286903c701d0f65c2e9b8afa
aba2rman <nikolayabaturov@gmail.com>
2023-03-12 18:44:20 +0700

    Добавьте файлы проекта.

---
+++ Abaturov_CSharp_Homework/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Homework1;
+
+public class Program
+{
+    public static void Main()
+    {
+        Console.Write("Введи свое имя: ");
+        string? name = Console.ReadLine(); // "?" указываем, так как string может иметь значение null 
+        SayHello(name);                    // Не до конца разобрался, но нашел это здесь: https://metanit.com/sharp/tutorial/2.15.php
+    }
+
+    public static void SayHello(string name)
+    {
+        Console.WriteLine($"Привет, {name}!");
+    }
+
+    /*  Если без использования метода, то выглядело бы так:
+     * 
+     *  Console.Write("Введи свое имя: ");
+     *  string? name = Console.ReadLine();
+     *  Console.WriteLine("Привет, " + name + "!"); */
+}
